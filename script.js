2d953791b93f9d68fed5e6eeb8e71183fb655f97
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2021-05-17 17:56:28 +0100

    zuri assignment

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict';
+let zuri = f => {
+  if (typeof f == 'number') {
+    let celsius = (f - 32) * (5 / 9);
+
+    return celsius;
+  } else if (typeof f == 'object') {
+    if (Array.isArray(f)) {
+      console.log('this is an array');
+
+      return `${f} is not a valid number but an Array`;
+    } else {
+      console.log('this is an object');
+      output = `${JSON.stringify(f)} is not a valid number but a ${typeof f}.`;
+      return output;
+    }
+  } else if (typeof f == 'string') {
+    console.log('');
+    let fNumber = Number(f);
+
+    console.log(fNumber);
+    console.log(typeof fNumber);
+
+    if (isNaN(fNumber)) {
+      console.log('this is not a number');
+      return `${f} is not a valid number but a ${typeof f}.`;
+    } else {
+      let celsius = (fNumber - 32) * (5 / 9);
+      console.log(celsius + ' c');
+      return celsius;
+    }
+  } else {
+    console.log('out of order');
+  }
+};
